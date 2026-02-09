cdda76dba525fcd4226375ca4b39e0ff93fc8280
Aziz Cotrim <azizcotrim8@gmail.com>
2026-02-09 18:15:39 -0300

    chore: adiciona database connection

---
+++ ListaDeTarefas/ListaDeTarefas.Infrastructure/Database/DataBase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace ListaDeTarefas.Infrastructure.Database
+{
+    public sealed class DataBase
+    {
+        private readonly string _conn;
+
+        public DataBase(string conn)
+        {
+            _conn = conn;
+        }
+
+        public SqlConnection GetSqlConnection()
+        {
+            SqlConnection conn = new SqlConnection(_conn);
+
+            return conn;
+        }
+
+    }
+}
